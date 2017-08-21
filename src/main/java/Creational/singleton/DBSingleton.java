148786166eafc29b0310ac5ffd732e6b5cb5109b
bradleyclarke <gce5>
2017-08-21 17:03:25 +0530

    threadsafe implementation

--- src/main/java/Creational/singleton/DBSingleton.java
+++ src/main/java/Creational/singleton/DBSingleton.java
@@ -4,17 +4,20 @@ public class DBSingleton {
 
   private static DBSingleton instance = null;
 
-  private DBSingleton(){
+  private DBSingleton() {
 
   }
 
-  public static DBSingleton getInstance(){
+  public static DBSingleton getInstance() {
     //lazily loading the singleton
-    if (instance == null){
-      instance = new DBSingleton();
+    if (instance == null) {
+      //adding threadsafe approach
+      synchronized (DBSingleton.class) {
+        if (instance == null) {
+          instance = new DBSingleton();
+        }
+      }
     }
-
     return instance;
   }
-
 }
