7eac4c53aaecdd56fdf523e52d543742bba55b13
bradleyclarke <gce5>
2017-08-21 17:23:16 +0530

    added derby db to the singleton demo

--- src/main/java/Creational/singleton/DBSingleton.java
+++ src/main/java/Creational/singleton/DBSingleton.java
@@ -1,11 +1,22 @@
 package Creational.singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.derby.jdbc.EmbeddedDriver;
+
 public class DBSingleton {
 
   private static DBSingleton instance = null;
 
-  private DBSingleton() {
+  private Connection conn = null;
 
+  private DBSingleton() {
+    try{
+      DriverManager.registerDriver(new EmbeddedDriver());
+    }catch (SQLException e){
+      e.printStackTrace();
+    }
   }
 
   public static DBSingleton getInstance() {
@@ -19,5 +30,22 @@ public class DBSingleton {
       }
     }
     return instance;
+  }
+
+  public Connection getConnection(){
+    if (conn ==null){
+      synchronized (DBSingleton.class){
+        if (conn ==null){
+          try{
+            String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+
+            conn = DriverManager.getConnection(dbUrl);
+          }catch (SQLException e){
+            e.printStackTrace();
+          }
+        }
+      }
+    }
+    return conn;
   }
 }
--- src/main/java/Creational/singleton/DBSingletonDemo.java
+++ src/main/java/Creational/singleton/DBSingletonDemo.java
@@ -1,5 +1,9 @@
 package Creational.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBSingletonDemo {
 
   public static void main(String[] args) {
@@ -10,8 +14,21 @@ public class DBSingletonDemo {
     DBSingleton anotherInstance = DBSingleton.getInstance();
 
     System.out.println(anotherInstance);
-
     //Observe that both of the objects printed are the same and hence only one of the object can be created
+
+    Connection conn = instance.getConnection();
+
+    Statement sta;
+    try{
+      sta = conn.createStatement();
+      int count = sta.executeUpdate("CREATE TABLE Address (ID INT, Streetname VARCHAR(20),"
+      + " City VARCHAR(20))");
+
+      System.out.println("Table Created");
+      sta.close();
+    }catch (SQLException e){
+      e.printStackTrace();
+    }
   }
 
 }
