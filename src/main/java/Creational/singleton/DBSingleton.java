7d6ff46f7e9bb4f5ac74aaddff7b5cad0cf296b8
bradleyclarke <gce5>
2017-08-21 16:55:08 +0530

    lazily loaded

--- src/main/java/Creational/singleton/DBSingleton.java
+++ src/main/java/Creational/singleton/DBSingleton.java
@@ -2,13 +2,18 @@ package Creational.singleton;
 
 public class DBSingleton {
 
-  private static DBSingleton instance = new DBSingleton();
+  private static DBSingleton instance = null;
 
   private DBSingleton(){
 
   }
 
   public static DBSingleton getInstance(){
+    //lazily loading the singleton
+    if (instance == null){
+      instance = new DBSingleton();
+    }
+
     return instance;
   }
 
