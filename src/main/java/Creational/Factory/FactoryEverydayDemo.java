4f7f34e683fc9dac2596543718a75e523b9feb5a
bradleyclarke <gce5>
2017-09-12 16:17:45 +0100

    factory every day demo added to the project

---
+++ src/main/java/Creational/Factory/FactoryEverydayDemo.java
@@ -0,0 +1,15 @@
+package Creational.Factory;
+
+import java.util.Calendar;
+
+public class FactoryEverydayDemo {
+
+  public static void main(String[] args) {
+    Calendar calendar = Calendar.getInstance();
+
+    System.out.println(calendar);
+
+    System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+  }
+
+}
